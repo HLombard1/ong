96387b4546453833a6d0a0c4621deffca11819a1
HenriqueLombardi <henriqueLombardi@estudante.sesisenai.org.br>
2026-04-08 12:00:31 -0300

    add - tudo

---
+++ script/script.js
@@ -0,0 +1,18 @@
+document.getElementById("formAdocao").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    let telefone = document.getElementById("telefone").value;
+    let cpf = document.getElementById("cpf").value;
+    let idade = document.getElementById("idade").value;
+    let cidade = document.getElementById("cidade").value;
+    let moradia = document.getElementById("moradia").value;
+    let quintal = document.querySelector('input[name="quintal"]:checked');
+    let pet = document.querySelector('input[name="pet"]:checked');
+    let h = document.getElementById("h").value;
+
+    if(nome.length < 3) return alert("nome invalido");
+
+    document.getElementById("resultado").innerHTML = "cadastro realizado com sucesso!<br>" + "Nome: " + nome;
+});
